db2e2cc6dd5604f40c4fcd570b2edb432a132771
Sam Kelly <sam@durosoft.com>
2016-12-07 23:26:41 -0500

    separate client/server, use a header file

--- build.sh
+++ build.sh
@@ -2,5 +2,4 @@
 if [ ! -f CImg.h ]; then
     ./scripts/get_cimg.sh
 fi
-g++ bettercast_client.cpp -L/usr/X11R6/lib -lm -lpthread -lX11 -std=c++11 -O3 || exit 1
-./a.out
+g++ bettercast_client.cpp -L/usr/X11R6/lib -lm -lpthread -lX11 -std=c++11 -O3 -o client || exit 1
