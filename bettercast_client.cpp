ce40f658520262526951503e319f9c9f0e55b739
Sam Kelly <sam@durosoft.com>
2016-12-07 23:15:55 -0500

    proof of concept client done

--- bettercast_client.cpp
+++ bettercast_client.cpp
@@ -74,18 +74,20 @@ void init_screen_buffer() {
   std::cout << "screen buffer initialized." << std::endl;
 }
 
-void foreach_screen_pixel(std::function<void (unsigned long&)> func) {
+void foreach_screen_pixel(std::function<void (unsigned long&, int&)> func) {
   XImage *image = XGetImage(display, root, 0, 0, screen_size.width, screen_size.height, AllPlanes, ZPixmap);
   //unsigned long red_mask = image->red_mask;
   //unsigned long green_mask = image->green_mask;
   //unsigned long blue_mask = image->blue_mask;
+  int i = 0;
   for(int x = 0; x < screen_size.width; x++)
   for(int y = 0; y < screen_size.height ; y++) {
     unsigned long pixel = XGetPixel(image, x, y);
     /*unsigned char b = pixel & blue_mask;
     unsigned char g = (pixel & green_mask) >> 8;
     unsigned char r = (pixel & red_mask) >> 16;*/
-    func(pixel);
+    func(pixel, i);
+    i++;
   }
 }
 
@@ -104,6 +106,7 @@ std::string read_msg(int socket) {
 }
 
 bool write_msg(int socket, std::string msg) {
+  std::cout << " >> " << msg << std::endl;
   std::string final_msg = msg + "\n";
   int n = write(socket, final_msg.c_str(), final_msg.size());
   if(n < 0) return false;
@@ -156,55 +159,30 @@ int main() {
     communicating = true;
     while(communicating) {
       std::string msg = read_msg(remote_sock);
-      std::cout << "received message: " << msg << std::endl;
+      std::cout << " << " << msg << std::endl;
       if(msg.size() < 2) msg = "close";
-      if(!write_msg(remote_sock, "generic response")) msg = "close";
       if(msg == "diff") { // send a diff
-
+        //TODO: implement
       } else if(msg == "key") { // send a keyframe
-
+        std::cout << "sending keyframe" << std::endl;
+        foreach_screen_pixel([&](unsigned long pixel, int i) {
+          screen_buffer[i] = pixel;
+        });
+        if(!write(remote_sock, screen_buffer, screen_buffer_size)) communicating = false;
+        if(!write_msg(remote_sock, "ok")) communicating = false;
       } else if(msg == "size") { // send the screen dimensions
         std::ostringstream os;
         os << screen_size.width << "x" << screen_size.height;
-        write_msg(remote_sock, os.str());
+        if(!write_msg(remote_sock, os.str())) communicating = false;
+      } else if(msg == "bettercast:probe") {
+        if(!write_msg(remote_sock, "bettercast:ack")) communicating = false;
       } else if(msg == "close") { // close the connection
-        std::cout << "the connection was closed" << std::endl;
         communicating = false;
-        shutdown(remote_sock, SHUT_RDWR);
-        close(remote_sock);
       }
     }
+    std::cout << "the connection was closed" << std::endl;
+    shutdown(remote_sock, SHUT_RDWR);
+    close(remote_sock);
   }
   close_sockets();
 }
-
-/*
-int main() {
-  int sockfd, newsockfd;
-  socklen_t client_len;
-  char buffer[256];
-  struct sockaddr_in server_address, client_address;
-  int n;
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if(sockfd < 0) error("ERROR opening socket");
-  bzero((char *) &server_address, sizeof(server_address));
-  server_address.sin_family = AF_INET;
-  server_address.slocal_addr.s_addr = INADDR_ANY;
-  server_address.sin_port = htons(BROADCAST_PORT_NUM);
-  if(bind(sockfd, (struct sockaddr *) &server_address, sizeof(server_address)) < 0)
-    error("ERROR on binding");
-  listen(sockfd,5);
-  client_len = sizeof(client_address);
-  newsockfd = accept(sockfd, (struct sockaddr *) &client_address, &client_len);
-  if (newsockfd < 0) error("ERROR on accept");
-  bzero(buffer, 256);
-  n = read(newsockfd, buffer, 255);
-  if(n < 0) error("ERROR reading from socket");
-  printf("Here is the message: %s\n", buffer);
-  n = write(newsockfd, "I got your message",18);
-  if(n < 0) error("ERROR writing to socket");
-  close(newsockfd);
-  close(sockfd);
-  return 0;
-}
-*/
